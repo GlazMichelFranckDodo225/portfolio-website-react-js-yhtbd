f63e8847155e24788030fd24fb8c59a181eed6ec
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-11-25 06:11:29 +0100

    Hero Section

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,5 +1,27 @@
 export default function HeroSection() {
-  return <h1>Hero Section</h1>;
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Salut, je suis Glaz DODO</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section--title-color">
+              Développeur Backend & Formateur</span>{""}
+            <br />
+            <span className="javaSpring">| Java - Spring</span>
+          </h1>
+          <p className="hero--section--description">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            <br />Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </div>
+        <button className="btn btn-primary">Contactez-moi</button>
+      </div>
+      <div className="hero--section--img">
+          <img src="./img/hero_img.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
 }
 
 
